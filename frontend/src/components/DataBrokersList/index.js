32401a9c56f03785e1f3df1623fd632239feb165
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2022-03-16 11:46:33 +0100

    typo

--- frontend/src/components/DataBrokersList/index.js
+++ frontend/src/components/DataBrokersList/index.js
@@ -165,7 +165,7 @@ const DataBrokersDB = ({ classes, dataBrokers }) => {
             >
               <AgGridColumn field="Domain" headerName="Service" minWidth={150} cellRenderer="domainCellRenderer"></AgGridColumn>
               <AgGridColumn field="Company Geo Country Code" headerName="Country" cellRenderer="countryCellRenderer" minWidth={95} maxWidth={110} ></AgGridColumn>
-              <AgGridColumn field="Company Name" headerName="Name" minWidth={100} ></AgGridColumn>
+              <AgGridColumn field="Company Name" headerName="Company Name" minWidth={100} ></AgGridColumn>
               <AgGridColumn field="Company Category Industry" headerName="Industry" minWidth={150} ></AgGridColumn>
               <AgGridColumn field="Company Category Sub Industry" headerName="Sub Industry" minWidth={150} ></AgGridColumn>
               <AgGridColumn field="YDR URL" headerName="Opt-out" cellRenderer="optOutCellRenderer" minWidth={95} maxWidth={100} ></AgGridColumn>
