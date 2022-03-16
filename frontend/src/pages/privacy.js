c62555ee1b7576ed4fa61a292b55e3ff8eeea3d1
Yoav Aviram <537092+yoavaviram@users.noreply.github.com>
2022-03-16 12:17:19 +0100

    Privacy policy update

--- frontend/src/pages/privacy.js
+++ frontend/src/pages/privacy.js
@@ -51,7 +51,7 @@ const Privacy = ({ classes, router }) => {
           <Typography gutterBottom={true}>
             <FormattedMessage id="privacy.lastUpdated" defaultMessage="Last updated:" />{" "}
             <FormattedDate
-              value={new Date(2021, 9, 15)}
+              value={new Date(2022, 3, 16)}
               year="numeric"
               month="long"
               day="2-digit"
@@ -64,45 +64,20 @@ const Privacy = ({ classes, router }) => {
           <Typography gutterBottom={true}>
             <FormattedMessage
               id="privacy.promiseBody"
-              defaultMessage="You own your data. We limit the Personal Data we collect from you to the absolute minimum required to deliver this Service (databrokerswatch.org). We will not sell or rent your personal information to anyone and will not retain it on our servers. We will only share your personal information when it is required to provide the service or as otherwise outlined in this Policy."
+              defaultMessage="We do not collect personal data. If we ever decide to collect personal data in the future then we will let you know in this Privacy Policy, and limit the Personal Data collected to the absolute minimum required to deliver this Service (databrokerswatch.org). We will never sell or rent your personal information to anyone."
             />
           </Typography>
           <br />
           <Typography gutterBottom={true} variant="body2">
             <FormattedMessage
               id="privacy.personalData"
-              defaultMessage="Personal Data That You Provide"
+              defaultMessage="Personal Data"
             />
           </Typography>
           <Typography gutterBottom={true}>
             <FormattedMessage
               id="privacy.personalDataBody1"
-              defaultMessage='While using this Service, we may ask you to provide us with certain personal information that can be used to contact or identify you ("Personal Data"). The Personally identifiable information collect is:'
-            />
-            <ul>
-              <li>
-                <FormattedMessage
-                  id="privacy.personalDataBodyOneC"
-                  defaultMessage="Cookies and Usage Data"
-                />
-              </li>
-            </ul>
-            <FormattedMessage
-              id="privacy.personalDataBodyOneE"
-              defaultMessage="By voluntarily providing us with Personal Data, you are consenting to our use of it in accordance with this Privacy Policy. If you provide Personal Data to this website, you acknowledge and agree that such Personal Data may be transferred from your current location to the offices and servers of yourdigitalrights.org and the authorized third parties referred to in this Policy."
-            />
-          </Typography>
-          <br />
-          <Typography gutterBottom={true} variant="body2">
-            <FormattedMessage
-              id="privacy.cookies"
-              defaultMessage="Cookies and Tracking"
-            />
-          </Typography>
-          <Typography gutterBottom={true}>
-            <FormattedMessage
-              id="privacy.cookiesBody"
-              defaultMessage='In operating the Service we use a technology called "cookies". A cookie is a piece of information that the computer hosting our Service gives to your browser when you access the Service. While we do not use cookies to deliver the Service, we use cookies to help us analyze Service usage more accurately, and to save the choices you make on the website so that it is easier for you to use the service next time. In all cases in which we use cookies, we will not collect Personal Data except with your explicit permission. On most web browsers, you will find a "help" section on the toolbar. Please refer to this section for information on how to receive notification when you are receiving a new cookie and how to turn cookies off.'
+              defaultMessage='We do not collect Personal Data :)'
             />
           </Typography>
           <br />
@@ -112,7 +87,7 @@ const Privacy = ({ classes, router }) => {
           <Typography gutterBottom={true}>
             <FormattedMessage
               id="privacy.usageDataBody"
-              defaultMessage='We may collect information on how the Service is accessed and used ("Usage Data"). This Usage Data may include information such as the Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages, the actions you perform, unique device identifiers and other diagnostic data. We use the following third-party Service Provider to monitor and analyze the use of our Service:'
+              defaultMessage='We may collect anonymous usage data to help us understand how to improve this Service ("Usage Data"). We use the following third-party Service Provider to monitor and analyze the use of our Service:'
             />
             <ul>
               <li>
